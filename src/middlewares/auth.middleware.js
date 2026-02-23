8bbfe758e27e563afea4c0d92b151bd5521f6292
unknown <neelkamall>
2026-02-23 12:51:05 +0530

    commit

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -5,7 +5,7 @@ import { User } from "../models/user.model.js";
 
 export const verifyjwt = asyncHandler(async(req, res, next) =>{
   try {
-    const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "")
+    const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer", "")
   
     if (!token) {
       throw new ApiError(401, "unauthorized request")
